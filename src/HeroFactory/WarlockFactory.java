2de5db790f6a17a5fd7532f3a035b71336b5c572
wailim0506 <wailim0506@icloud.com>
2024-11-09 17:03:01 +0800

    remove unnecessary comment

--- src/HeroFactory/WarlockFactory.java
+++ src/HeroFactory/WarlockFactory.java
@@ -5,12 +5,6 @@ import java.util.*;
 
 public class WarlockFactory implements HeroFactory {
     public Hero createHero(Scanner sc, String id, String name) {
-        //should put in main
-//        System.out.print("Please input hero information (id, name):- ");
-//        String idName = sc.nextLine();
-//        String[] split = idName.split(", ");
-//        String id = split[0];
-//        String name = split[1];
         return new Warlock(id, name);
     }
 }
--- src/HeroFactory/WarriorFactory.java
+++ src/HeroFactory/WarriorFactory.java
@@ -4,12 +4,6 @@ import Hero.*;
 import java.util.*;
 public class WarriorFactory implements HeroFactory {
     public Hero createHero(Scanner sc, String id, String name) {
-        //should put in main
-//        System.out.print("Please input hero information (id, name):- ");
-//        String idName = sc.nextLine();
-//        String[] split = idName.split(", ");
-//        String id = split[0];
-//        String name = split[1];
         return new Warrior(id, name);
     }
 }
